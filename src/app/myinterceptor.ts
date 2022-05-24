66c3125046923d772510f3ef8fba12cf107ce403
QHP1015 <2439462872@qq.com>
2022-05-24 11:09:10 +0800

    fix: 修改拦截器
    拦截器只拦截请求，不拦截响应
    
    BREAKING CHANGE: 删除响应代码

--- src/app/myinterceptor.ts
+++ src/app/myinterceptor.ts
@@ -23,38 +23,38 @@ export class MyInterceptor implements HttpInterceptor {
         let authReq: any;
         // 实现不拦截的方式：1. 指定接口不拦截  2. 判断本地sessionStorage
         if (!req.url.includes('login' || 'register')) {
-            const token:any = sessionStorage.getItem('token');
-            console.log(token);
-            authReq = req.clone({setHeaders: {token}});
+            const Authorization:any = `Bearer [${sessionStorage.getItem('token')}]`;
+            console.log(Authorization);
+            authReq = req.clone({setHeaders: {Authorization}});
             return next.handle(authReq).pipe(
-                mergeMap((event: any) => {
-                    // 允许统一对请求错误处理
-                    if (event instanceof HttpResponse) {
-                        const body: any = event.body;
-                        console.log(body);
-                        if (body.code !== 200) {
-                            // 继续抛出错误中断后续所有 Pipe、subscribe 操作，因此：
-                            // this.http.get('/').subscribe() 并不会触发
-                            if (body.code === 401) {// token过期，并且无法刷新，需要重新登录
-                                this.router.navigate(['/login']);
-                            } else if (body.code === 100) {// token过期，服务器端自动刷新了token，需要用新的token重新发送请求
-                                const newToken = body.token;
-                                sessionStorage.setItem('token', newToken);
-                                console.log('new token: ' + newToken);
-                                authReq = req.clone({setHeaders: {token: newToken}});
-                                return next.handle(authReq).pipe();
-                            }
-                            return throwError({});
-                        } else {
-                            // 重新修改 `body` 内容为 `response` 内容，对于绝大多数场景已经无须再关心业务状态码
-                            // return of(new HttpResponse(Object.assign(event, { body: body.response })));
-                            // 或者依然保持完整的格式
-                            return of(event);
-                        }
-                    } else {
-                        return of(event);
-                    }
-                }),
+                // mergeMap((event: any) => {
+                //     // 允许统一对请求错误处理
+                //     if (event instanceof HttpResponse) {
+                //         const body: any = event.body;
+                //         console.log(body);
+                //         if (body.code !== 200) {
+                //             // 继续抛出错误中断后续所有 Pipe、subscribe 操作，因此：
+                //             // this.http.get('/').subscribe() 并不会触发
+                //             if (body.code === 401) {// token过期，并且无法刷新，需要重新登录
+                //                 this.router.navigate(['/login']);
+                //             } else if (body.code === 100) {// token过期，服务器端自动刷新了token，需要用新的token重新发送请求
+                //                 const newToken = body.token;
+                //                 sessionStorage.setItem('token', newToken);
+                //                 console.log('new token: ' + newToken);
+                //                 authReq = req.clone({setHeaders: {token: newToken}});
+                //                 return next.handle(authReq).pipe();
+                //             }
+                //             return throwError({});
+                //         } else {
+                //             // 重新修改 `body` 内容为 `response` 内容，对于绝大多数场景已经无须再关心业务状态码
+                //             // return of(new HttpResponse(Object.assign(event, { body: body.response })));
+                //             // 或者依然保持完整的格式
+                //             return of(event);
+                //         }
+                //     } else {
+                //         return of(event);
+                //     }
+                // }),
                 catchError((err: HttpErrorResponse) => this.handleData(err))
             );
         }
